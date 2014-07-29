91a53e932677fecafb69810f44592c2fb00468cb
Lobisomen <renegadospain@gmail.com>
2014-07-29 16:17:13 +0200

    Create infopanel.startup.lua

---
+++ infopanel.startup.lua
@@ -0,0 +1,38 @@
+print("Waiting 10 seconds so we don't try and wrap peripherals before they're ready")
+sleep(10)
+print()
+local rnd = math.random(20)
+print("Also waiting a random amount of time ("..rnd.."sec.) so we don't slam the server with tons of HTTP requests")
+print()
+sleep(rnd)
+
+-- install any updates to this startup script
+print("Updating startup script")
+local start_src = http.get("https://raw.github.com/Lobisomen/sg_worlds/master/infopanel_startup.lua")
+if start_src ~= nil then
+	local file = fs.open("/startup","w")
+	file.write(start_src.readAll())
+	file.close()
+	start_src.close()
+	print("Startup script updated successfully")
+else
+	print("Failed to update startup script")
+end
+
+print()
+
+-- install any updates to SGInfo
+print("Updating SGInfo")
+local sgc_src = http.get("https://raw.github.com/Lobisomen/sg_worlds/master/SGInfo.lua")
+if sgc_src ~= nil then
+	local file = fs.open("/SGInfo","w")
+	file.write(sgc_src.readAll())
+	file.close()
+	sgc_src.close()
+	print("SGInfo updated successfully")
+else
+	print("Failed to update SGInfo")
+end
+print()
+print("Starting up SGInfo")
+shell.run("SGInfo")
